ef7e1f989e7d400fe2b1b0abdaa44e4117e8356a
nandalories <125984274+nandalories@users.noreply.github.com>
2023-04-19 11:46:44 +0300

    Create temp_start.py

---
+++ temp_start.py
@@ -0,0 +1,5 @@
+temp_start = float(input("Введите начальную температуру: "))
+temp_end = float(input("Введите конечную температуру: "))
+time = float(input("Введите время в секундах: "))
+
+temp_diff = temp_end - temp_start
